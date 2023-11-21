4eb2388058f5e1269db647e3bfa4a19a2bbb02fb
Andy Tran <andy.tran@tlgcohort.com>
2023-11-21 10:56:20 -0800

    new tests

--- test/io/trivia/QuestionTest.java
+++ test/io/trivia/QuestionTest.java
@@ -2,31 +2,23 @@ package io.trivia;
 
 import org.junit.Before;
 import org.junit.Test;
-
 import java.util.*;
-
 import static org.junit.Assert.*;
 
 public class QuestionTest {
-    @Before
-    public void init() {
-        List<String> tester = new ArrayList<String>(Arrays.asList("body", "answer", "wrongchoice1", "wrongchoice2", "wrongchoice3"));
-        Question question = new Question(tester);
-    }
-
-    @Test
-    public void allChoicesRandom_shouldReturnAnswerChoicesInDifferentOrder_whenCalled() {
-        List<String> tester = new ArrayList<>(Arrays.asList("body", "answer", "wrongchoice1", "wrongchoice2", "wrongchoice3"));
-        Question question = new Question(tester);
-        System.out.println(question.allChoicesRandom());
-
-    }
 
     @Test
     public void wrongChoices_shouldReturnListOfLenThree_whenCalled() {
         List<String> tester = new ArrayList<>(Arrays.asList("body", "answer", "wrongchoice1", "wrongchoice2", "wrongchoice3"));
         Question question = new Question(tester);
-        assertEquals(question.wrongChoices().size(), 3.0, .001);
+        assertEquals(question.wrongChoices().size(), 3);
+    }
+
+    @Test
+    public void allChoicesRandom_shouldReturnListLenFour_whenCalled() {
+        List<String> tester = new ArrayList<>(Arrays.asList("body", "answer", "wrongchoice1", "wrongchoice2", "wrongchoice3"));
+        Question question = new Question(tester);
+        assertEquals(question.allChoicesRandom().size(), 4);
     }
 
     @Test
