cdf118f6864c6fb4bb9cef4575d4f051f4cfb3ec
oshayjackson <oshay.jackson@tlgcohort.com>
2023-11-20 22:17:06 -0800

    adjusted the code to be a little more neat and inserted some spacing between name prompts

--- src/io/trivia/app/GameHost.java
+++ src/io/trivia/app/GameHost.java
@@ -1,45 +1,65 @@
 package io.trivia.app;
 
+import com.apps.util.Console;
 import com.apps.util.Prompter;
-import io.trivia.Category;
 import io.trivia.Player;
 import io.trivia.Question;
-import org.apache.commons.lang3.EnumUtils;
 
-import java.util.*;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+//~oshay collapsed imports
 
 public class GameHost {
-
-    public void startGame() {
-        Prompter prompter = new Prompter(new Scanner(System.in));
-        String runAgain = "y";
-        while (runAgain.equals("y")) {
-            String name = namePromptPlayer1();
-            String name2 = namePromptPlayer2();
-            String category = Question.getCategoryFromUserInput();
-            Player player = new Player(name);
-            Player player2 = new Player(name2);
-            playGame(category, player, player2);
-            endGameMessage(player, player2);
-            runAgain = prompter.prompt("Press 'y' to play again, any other character to exit. ");
-        }
-    }
-
-    private void playGame(String category, Player player, Player player2) {
-        Set<String> askedQuestions = new HashSet<>();
-        Question.askQuestionOps(askedQuestions, player, player2, category);
-    }
-
-    public String namePromptPlayer1() {
-        Prompter prompter = new Prompter(new Scanner(System.in));
-        return prompter.prompt("Player 1 please enter your name: ");
-    }
-
-    public String namePromptPlayer2() {
-        Prompter prompter = new Prompter(new Scanner(System.in));
-        return prompter.prompt("Player 2 please enter your name: ");
-    }
-
+	Console console; //~oshay added this
+	
+	public void startGame() {
+		Prompter prompter = new Prompter(new Scanner(System.in));
+		String runAgain = "Y";
+		while(runAgain.equals("Y")) {
+			String name = namePromptPlayer1();
+			String name2 = namePromptPlayer2();
+			String category = Question.getCategoryFromUserInput();
+			Player player = new Player(name);
+			Player player2 = new Player(name2);
+			playGame(category, player, player2);
+			endGameMessage(player, player2);
+			runAgain = prompter.prompt("Press 'y' to play again, any other character to exit. ");
+		}
+	}
+	
+	private void playGame(String category, Player player, Player player2) {
+		Set <String> askedQuestions = new HashSet <>();
+		Question.askQuestionOps(askedQuestions, player, player2, category);
+	}
+	
+	public String namePromptPlayer1() {
+		Prompter prompter = new Prompter(new Scanner(System.in));
+		Console.blankLines(1); //~oshay added this
+		return prompter.prompt("⭕️ ░░░░░░░░▒▒▒▒▒▒▒▒▒▒▒▓▓▓▓▓▓▓▓▓▓ 😎 PLAYER ① Enter your name: "); //~oshay added this
+	}
+	
+	public String namePromptPlayer2() {
+		Prompter prompter = new Prompter(new Scanner(System.in));
+		Console.blankLines(1); //~oshay added this
+		return prompter.prompt("⭕️ ░░░░░░░░▒▒▒▒▒▒▒▒▒▒▒▓▓▓▓▓▓▓▓▓▓ 😎 PLAYER ②  Enter your name: "); //~oshay added this
+	}
+	
+	
+	public void endGameMessage(Player player, Player player2) {
+		System.out.println(player.getName() + "'s score: " + player.getScore());
+		System.out.println(player2.getName() + "'s score: " + player2.getScore());
+		if(player.getScore() == player2.getScore()) {
+			System.out.println("Tie game!!!");
+		}
+		else if(player.getScore() > player2.getScore()) {
+			System.out.println(player.getName() + " 🎊🥇Is the winner🥇🎉");
+		}
+		else {
+			System.out.println(player2.getName() + " 🎊🥇Is the winner🥇🎉");
+		}
+	}
+	/*
 //    public String categoryPrompt() {
 //        Prompter prompter = new Prompter(new Scanner(System.in));
 //        String category;
@@ -54,17 +74,5 @@ public class GameHost {
 //                return category;
 //            }
 //        }
-//    }
-
-    public void endGameMessage(Player player, Player player2) {
-        System.out.println(player.getName() + "'s score: " + player.getScore());
-        System.out.println(player2.getName() + "'s score: " + player2.getScore());
-        if (player.getScore() == player2.getScore()) {
-            System.out.println("Tie game!!!");
-        } else if (player.getScore() > player2.getScore()) {
-            System.out.println(player.getName() + " wins!!!");
-        } else {
-            System.out.println(player2.getName() + " wins!!!");
-        }
-    }
+//    }*/// commented out idk why ~oshay
 }
